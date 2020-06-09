c3a58883c1cec805c1e0a602962638f633925e5b
frdazzi <frdazzi@gmail.com>
2020-06-09 19:45:34 -0300

    Ajuste GD e início do Projeto Final.

--- gd/exemplo-03.php
+++ gd/exemplo-03.php
@@ -6,9 +6,13 @@
 
     // Funcao imagettftext
     // Parametros: image, tamanho da fonte, angulo de inclinacao do texto, eixo-x, eixo-y, cor, caminho da font, texto
-    imagettftext($image, 32, 0, 450, 150, $titleColor,"fonts".DIRECTORY_SEPARATOR."Bevan".DIRECTORY_SEPARATOR."Bevan-Regular.ttf","CERTIFICADO");
-    imagettftext($image, 32, 0, 450, 150, $titleColor,"fonts".DIRECTORY_SEPARATOR."Playball".DIRECTORY_SEPARATOR."Playball-Regular.ttf","CERTIFICADO");
-    imagestring($image, 3, 440, 370, utf8_decode("Concluído em: ").date("d/m/Y"), $titleColor);
+    imagettftext($image, 32, 0, 320, 150, $titleColor,__DIR__.DIRECTORY_SEPARATOR."fonts".DIRECTORY_SEPARATOR."Bevan".DIRECTORY_SEPARATOR."Bevan-Regular.ttf",
+    "CERTIFICADO");
+    imagettftext($image, 32, 0, 310, 350, $titleColor,__DIR__.DIRECTORY_SEPARATOR."fonts".DIRECTORY_SEPARATOR."Playball".DIRECTORY_SEPARATOR."Playball-Regular.ttf",
+    "Fábio Rosalem Dazzi");
+    imagestring($image, 3, 400, 370, utf8_decode("Concluído em: ").date("d/m/Y"), $titleColor);
+    //imagettftext($image, 32, 0, 450, 150, $titlecolor, __DIR__.DIRECTORY_SEPARATOR."fonts".DIRECTORY_SEPARATOR."Bevan".DIRECTORY_SEPARATOR."Bevan-Regular.ttf", "Certificado");
+
 
 
     header("Content-type: image/jpeg");
